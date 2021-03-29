6acb3b9db568d228448a167e8fc4b97d8dcb4264
Emman <emmanueldubria@gmail.com>
2021-03-30 00:01:06 +0800

    Added meta tag for apple device

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ export default function APV({ Component, pageProps }) {
     <>
       <Head>
         <title>AS PER VISUAL</title>
+      	  <meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
     </>
