783a96b9833ecbd127cd953bcf030ec18a57ec31
Emman <emmanueldubria@gmail.com>
2021-03-30 09:18:48 +0800

    Added meta view port

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,8 @@ export default function APV({ Component, pageProps }) {
         <title>AS PER VISUAL</title>
       	  <meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="mobile-web-app-capable" content="yes" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
       </Head>
       <Component {...pageProps} />
     </>
