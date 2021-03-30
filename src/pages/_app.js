f6fff36374560a60fc32db93961ea7059d053fb8
Emman <emmanueldubria@gmail.com>
2021-03-30 10:12:27 +0800

    Added letter spacing on heading

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,9 @@ export default function APV({ Component, pageProps }) {
     <>
       <Head>
         <title>AS PER VISUAL</title>
-	  <meta name="viewport" content="width=device-width, initial-scale=1">
+		      <meta name="viewport" content="width=device-width, initial-scale=1">
       	  <meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="mobile-web-app-capable" content="yes" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		      <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
     </>
