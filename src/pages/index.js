215eb49b1bc4bc8b6d0a1fedabd37a65e749769c
Emman <emmanueldubria@gmail.com>
2021-04-03 19:38:12 +0800

    Removed controls on video, replace iframe to video tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,7 +91,9 @@ export default function Home() {
                                     <div className={styles.videoContainer}>
 
                                         <div id="yt-iframe1" className={styles.videoInIframe}>
-                                            <iframe src="/video/AS PER VISUAL - INTRO VIDEO - FINAL.mp4" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                            <video loop="true" autoplay="autoplay" id="vid">
+                                            	<source src="/video/AS PER VISUAL - INTRO VIDEO - FINAL.mp4" type="video/mp4"/>
+                                            </video>
                                         </div>
                                     </div>
                                 </div>
